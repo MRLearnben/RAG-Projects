901f4ac15a5bc7b7be2375c21a91cdc5260fad83
Thilak <108942764+MRLearnben@users.noreply.github.com>
2026-04-10 15:00:06 +0530

    pdf_loader.py

---
+++ pdf_loader.py
@@ -0,0 +1,14 @@
+#pdf_loader.py
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_and_split_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=100
+    )
+
+    return splitter.split_documents(documents)
